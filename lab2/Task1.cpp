bafeea77fc32d0356abe5385179309f028c2eb2e
Шоола Табылдиева <shoola.tabyldieva@gmail.com>
2024-01-29 23:42:50 +0600

    add report for lab2

--- lab2/Task1.cpp
+++ lab2/Task1.cpp
@@ -12,7 +12,6 @@
 //    std::string departureTime;
 //    int registrationSection;
 //
-//    // Class constructor
 //    Aeroflot(int number, const std::string& departure, const std::string& destination,
 //        const std::string& arrival, const std::string& departureTime, int section)
 //        : flightNumber(number),
@@ -93,145 +92,143 @@
 //    return 0;
 //}
 
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//
-//class Student {
-//public:
-//    std::string firstName;
-//    std::string lastName;
-//    std::string birthDate;
-//    std::string phoneNumber;
-//
-//    // Конструктор класса Student
-//    Student(const std::string& first, const std::string& last, const std::string& birth, const std::string& phone)
-//        : firstName(first), lastName(last), birthDate(birth), phoneNumber(phone) {}
-//
-//    // Вывод информации о студенте
-//    void displayStudentInfo() const {
-//        std::cout << "Name: " << firstName << " " << lastName << "\tBirthdate: " << birthDate << "\tPhone: " << phoneNumber << std::endl;
-//    }
-//};
-//
-//class StudentGroup {
-//private:
-//    std::vector<Student> students;
-//
-//public:
-//    void addStudent(const Student& student) {
-//        students.push_back(student);
-//    }
-//
-//    void removeStudentByLastName(const std::string& lastName) {
-//        students.erase(std::remove_if(students.begin(), students.end(),
-//            [lastName](const Student& student) { return student.lastName == lastName; }), students.end());
-//    }
-//
-//    void findStudentByLastName(const std::string& lastName) const {
-//        for (const auto& student : students) {
-//            if (student.lastName == lastName) {
-//                std::cout << "Student found: ";
-//                student.displayStudentInfo();
-//                return;
-//            }
-//        }
-//        std::cout << "Student with last name '" << lastName << "' not found." << std::endl;
-//    }
-//
-//    void sortStudentsByLastName() {
-//        std::sort(students.begin(), students.end(), [](const Student& a, const Student& b) {
-//            return a.lastName < b.lastName;
-//            });
-//    }
-//
-//    void displayAllStudents() const {
-//        std::cout << "\nAll students in the group:\n";
-//        for (const auto& student : students) {
-//            student.displayStudentInfo();
-//        }
-//        std::cout << std::endl;
-//    }
-//};
-//
-//int main() {
-//    StudentGroup group;
-//
-//    group.addStudent(Student("Shoola", "Tabyldieva", "01-02-2003", "234566543345"));
-//    group.addStudent(Student("Ksenia", "Park", "12-02-2004", "23456765433456"));
-//    group.addStudent(Student("Alex", "Johnson", "14-06-2003", "3456788765"));
-//
-//    int choice;
-//    std::string lastName;
-//
-//    do {
-//        std::cout << "\n****************************";
-//        std::cout << "\nStudent Group Menu:\n"
-//            << "1. Display all students\n"
-//            << "2. Add student\n"
-//            << "3. Remove student by last name\n"
-//            << "4. Find student by last name\n"
-//            << "5. Sort students by last name\n"
-//            << "6. Exit\n";
-//            std::cout << "****************************" << std::endl;
-//
-//            std::cout<< "Enter your choice: ";
-//        std::cin >> choice;
-//        std::cout << std::endl;
-//
-//        switch (choice) {
-//        case 1:
-//            group.displayAllStudents();
-//            break;
-//
-//        case 2:
-//        {
-//            std::string first, last, birth, phone;
-//            std::cout << "Enter student information:\n";
-//            std::cout << "First name: ";
-//            std::cin >> first;
-//            std::cout << "Last name: ";
-//            std::cin >> last;
-//            std::cout << "Birthdate: ";
-//            std::cin >> birth;
-//            std::cout << "Phone number: ";
-//            std::cin >> phone;
-//            group.addStudent(Student(first, last, birth, phone));
-//            std::cout << "Student added successfully.\n";
-//        }
-//        break;
-//
-//        case 3:
-//            std::cout << "Enter the last name of the student to remove: ";
-//            std::cin >> lastName;
-//            group.removeStudentByLastName(lastName);
-//            std::cout << "Student removed successfully.\n";
-//            break;
-//
-//        case 4:
-//            std::cout << "Enter the last name of the student to find: ";
-//            std::cin >> lastName;
-//            group.findStudentByLastName(lastName);
-//            break;
-//
-//        case 5:
-//            group.sortStudentsByLastName();
-//            std::cout << "Students sorted by last name.\n";
-//            break;
-//
-//        case 6:
-//            std::cout << "Exiting the program.\n";
-//            break;
-//
-//        default:
-//            std::cout << "Invalid choice. Please enter a valid option.\n";
-//            break;
-//        }
-//
-//    } while (choice != 0);
-//
-//    return 0;
-//}
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Student {
+public:
+    std::string firstName;
+    std::string lastName;
+    std::string birthDate;
+    std::string phoneNumber;
+
+    Student(const std::string& first, const std::string& last, const std::string& birth, const std::string& phone)
+        : firstName(first), lastName(last), birthDate(birth), phoneNumber(phone) {}
+
+    void displayStudentInfo() const {
+        std::cout << "Name: " << firstName << " " << lastName << "\tBirthdate: " << birthDate << "\tPhone: " << phoneNumber << std::endl;
+    }
+};
+
+class StudentGroup {
+private:
+    std::vector<Student> students;
+
+public:
+    void addStudent(const Student& student) {
+        students.push_back(student);
+    }
+
+    void removeStudentByLastName(const std::string& lastName) {
+        students.erase(std::remove_if(students.begin(), students.end(),
+            [lastName](const Student& student) { return student.lastName == lastName; }), students.end());
+    }
+
+    void findStudentByLastName(const std::string& lastName) const {
+        for (const auto& student : students) {
+            if (student.lastName == lastName) {
+                std::cout << "Student found: ";
+                student.displayStudentInfo();
+                return;
+            }
+        }
+        std::cout << "Student with last name '" << lastName << "' not found." << std::endl;
+    }
+
+    void sortStudentsByLastName() {
+        std::sort(students.begin(), students.end(), [](const Student& a, const Student& b) {
+            return a.lastName < b.lastName;
+            });
+    }
+
+    void displayAllStudents() const {
+        std::cout << "\nAll students in the group:\n";
+        for (const auto& student : students) {
+            student.displayStudentInfo();
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    StudentGroup group;
+
+    group.addStudent(Student("Shoola", "Tabyldieva", "01-02-2003", "234566543345"));
+    group.addStudent(Student("Ksenia", "Park", "12-02-2004", "23456765433456"));
+    group.addStudent(Student("Alex", "Johnson", "14-06-2003", "3456788765"));
+
+    int choice;
+    std::string lastName;
+
+    do {
+        std::cout << "\n****************************";
+        std::cout << "\nStudent Group Menu:\n"
+            << "1. Display all students\n"
+            << "2. Add student\n"
+            << "3. Remove student by last name\n"
+            << "4. Find student by last name\n"
+            << "5. Sort students by last name\n"
+            << "6. Exit\n";
+            std::cout << "****************************" << std::endl;
+
+            std::cout<< "Enter your choice: ";
+        std::cin >> choice;
+        std::cout << std::endl;
+
+        switch (choice) {
+        case 1:
+            group.displayAllStudents();
+            break;
+
+        case 2:
+        {
+            std::string first, last, birth, phone;
+            std::cout << "Enter student information:\n";
+            std::cout << "First name: ";
+            std::cin >> first;
+            std::cout << "Last name: ";
+            std::cin >> last;
+            std::cout << "Birthdate: ";
+            std::cin >> birth;
+            std::cout << "Phone number: ";
+            std::cin >> phone;
+            group.addStudent(Student(first, last, birth, phone));
+            std::cout << "Student added successfully.\n";
+        }
+        break;
+
+        case 3:
+            std::cout << "Enter the last name of the student to remove: ";
+            std::cin >> lastName;
+            group.removeStudentByLastName(lastName);
+            std::cout << "Student removed successfully.\n";
+            break;
+
+        case 4:
+            std::cout << "Enter the last name of the student to find: ";
+            std::cin >> lastName;
+            group.findStudentByLastName(lastName);
+            break;
+
+        case 5:
+            group.sortStudentsByLastName();
+            std::cout << "Students sorted by last name.\n";
+            break;
+
+        case 6:
+            std::cout << "Exiting the program.\n";
+            break;
+
+        default:
+            std::cout << "Invalid choice. Please enter a valid option.\n";
+            break;
+        }
+
+    } while (choice != 0);
+
+    return 0;
+}
 
 #include <iostream>
 #include <fstream>
